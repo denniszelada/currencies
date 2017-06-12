77a10a45070e52b6f3878b56cb236fdac613c680
Dennis Zelada <dennis.zelada@modomoto.de>
2017-06-12 17:06:33 +0200

    Add url and values to crypto_compare_client_spec

--- spec/services/crypto_compare_client_spec.rb
+++ spec/services/crypto_compare_client_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe CryptoCompareClient do
   it 'fetches the historic value of bitcoin' do
-    stub_request(:get, '').with({}.to_json)
+    stub_request(:get, 'https://min-api.cryptocompare.com/data/histoday').
+      with(body: {fsym:'BTC', tsym:'USD', limit:'60', aggregate:'3', e:'CCCAGG'}.to_json)
 
     values = CryptoCompareClient.fetch_bitcoin
 
